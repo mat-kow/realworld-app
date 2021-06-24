56e397eefa2734c9d643c31299fc972244237fdf
unknown <mateokow90@gmail.com>
2021-06-24 11:23:11 +0200

    h2 config

---
+++ src/main/java/pl/teo/realworldstarterkit/RealworldStarterKitApplication.java
@@ -0,0 +1,13 @@
+package pl.teo.realworldstarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldStarterKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/teo/realworldstarterkit/RealworldStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.teo.realworldstarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldStarterKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
